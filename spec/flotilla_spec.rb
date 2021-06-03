b83076437717f86522387132caaf009ec623f488
Amanda McGuire <aruwart001@gmail.com>
2021-06-03 10:13:46 -0600

    Add iteration 3 test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -83,4 +83,51 @@ RSpec.describe Flotilla do
       expect(@seventh_flotilla.recommend_personnel(@odyssey)).to eq([@polly])
     end
   end
+
+  describe "Iteration 3" do
+    before(:each) do
+      @daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      @daedalus.add_requirement({astrophysics: 6})
+      @daedalus.add_requirement({quantum_mechanics: 3})
+      @odyssey = Spacecraft.new({name: 'Odyssey', fuel: 300})
+      @odyssey.add_requirement({operations: 6})
+      @odyssey.add_requirement({maintenance: 3})
+      @seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      @kathy = Person.new('Kathy Chan', 10)
+      @kathy.add_specialty(:astrophysics)
+      @kathy.add_specialty(:quantum_mechanics)
+      @polly = Person.new('Polly Parker', 8)
+      @polly.add_specialty(:operations)
+      @polly.add_specialty(:maintenance)
+      @rover = Person.new('Rover Henriette', 1)
+      @rover.add_specialty(:operations)
+      @rover.add_specialty(:maintenance)
+      @sampson = Person.new('Sampson Edwards', 7)
+      @sampson.add_specialty(:astrophysics)
+      @sampson.add_specialty(:quantum_mechanics)
+      @seventh_flotilla.add_personnel(@kathy)
+      @seventh_flotilla.add_personnel(@polly)
+      @seventh_flotilla.add_personnel(@rover)
+      @seventh_flotilla.add_personnel(@sampson)
+    end
+
+    it "starts off with no ships" do
+      expect(@seventh_flotilla.ships).to eq([])
+    end
+
+    it "can add ships" do
+      @seventh_flotilla.add_ship(@daedalus)
+      @seventh_flotilla.add_ship(@odyssey)
+
+      expect(@seventh_flotilla.ships).to eq([@daedalus, @odyssey])
+    end
+
+    it "can tell personnel by ship" do
+      expected = {
+                  @daedalus => [@kathy, @sampson],
+                  @odyssey => [@polly]
+                }
+      expect(@seventh_flotilla.personnel_by_ship).to eq(expected)
+    end
+  end
 end
