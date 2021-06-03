99d0f60abc1006f8ff915be522523a656a59b0cc
Amanda McGuire <aruwart001@gmail.com>
2021-06-03 09:04:59 -0600

    create spacecraft test

---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,16 @@
+require './lib/spacecraft'
+
+RSpec.describe Spacecraft do
+  it "exists" do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus).to be_a(Spacecraft)
+  end
+
+  it "has attributes" do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus.name).to eq('Daedalus')
+    expect(daedalus.fuel).to eq(400)
+  end
+end
